4fc4cc972689b3a9bb3c2c7fd895a10ca91e4a5c
hashemsh <sh.sheikhypour@gmail.com>
2020-01-30 15:08:54 +0330

    fix layout and view

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/bootstrap-rtl.min.css">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>کوتاه کننده لینک</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            @yield('content')
+        </div>
+    </div>
+</div>
+<script src="/js/jquery-3.4.1.slim.min.js"></script>
+<script src="/js/bootstrap-rtl.min.js"></script>
+</body>
+</html>
