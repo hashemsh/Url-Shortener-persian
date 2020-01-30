e6a138cbcea95d70ad6be04212810eaae24647e9
hashemsh <sh.sheikhypour@gmail.com>
2020-01-30 15:30:29 +0330

    make model for Url and controller

---
+++ database/migrations/2020_01_30_115735_create_urls_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUrlsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('urls', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('main_url');
+            $table->text('short_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('urls');
+    }
+}
