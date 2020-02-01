919b8aafcf3b147416eb2f4b2310b115675a4ecb
hashemsh <sh.sheikhypour@gmail.com>
2020-02-01 18:54:14 +0330

    add special link input to view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,12 @@
         {{ csrf_field() }}
         <label for="url" class="text-center mb-5">لینک کامل را وارد کنید </label>
         <input type="text" name="url" id="url" class="form-control mb-5">
+        <div class="form-group">
+            <label class="specialLabel" for="specialUrl">لینک اختصاصی </label>
+            <input type="text" name="specialUrl" id="specialUrl" class="form-control mb-5 specialInput" aria-describedby="specialHelp">
+            <small id="specialHelp" class="form-text text-muted">در غیر اینصورت سیستم به صورت رندوم برای شما لینک میسازد</small>
+        </div>
+        <input type="submit" name="submit" class="btn btn-lg btn-outline-success" value="ثبت لینک">
     </form>
     <div class="col-12 mt-5 w-75 m-auto">
         @if($errors->any())
